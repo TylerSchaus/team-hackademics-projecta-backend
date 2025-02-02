5bc93421f60647860c8db1d85f8b6b18909812b2
TylerSchaus <tylerdschaus@shaw.ca>
2025-02-01 18:43:14 -0800

    Initial Project Skeleton

---
+++ Projecta-Backend/src/main/java/com/hackademics/ProjectaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hackademics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectaBackendApplication.class, args);
+	}
+
+}
---
+++ Projecta-Backend/src/test/java/com/hackademics/ProjectaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackademics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
