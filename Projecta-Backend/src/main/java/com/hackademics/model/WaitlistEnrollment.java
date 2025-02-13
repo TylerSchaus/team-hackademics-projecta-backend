06922e5ba3a77eb18fa3787a3eec9c6eda6407b7
nadeenhamdona <nadeenhamdona2@gmail.com>
2025-02-12 16:35:32 -0800

    Redo pull request for Entity and Repository

--- Projecta-Backend/src/main/java/com/hackademics/model/WaitlistEnrollment.java
+++ Projecta-Backend/src/main/java/com/hackademics/model/WaitlistEnrollment.java
@@ -1,5 +1,45 @@
 package com.hackademics.Model;
 
-public class WaitlistEnrollment {
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.io.Serializable;
+
+@Entity
+@Table(name = "waitlist_enrollments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WaitlistEnrollment implements Serializable {
+
+    // Composite key fields
+    @EmbeddedId
+    private WaitlistEnrollmentId id;
+
+    @Column(name = "waitlist_position", nullable = false)
+    private int waitlistPosition;
+
+    @ManyToOne
+    @MapsId("waitlistId")
+    @JoinColumn(name = "waitlist_id")
+    private Waitlist waitlist;
+
+    @ManyToOne
+    @MapsId("studentId")
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    // Composite key class (embedded inside WaitlistEnrollment)
+    @Embeddable
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class WaitlistEnrollmentId implements Serializable {
+
+        @Column(name = "waitlist_id", length = 5)
+        private String waitlistId;
+
+        @Column(name = "student_id", length = 9)
+        private String studentId;
+    }
 }
