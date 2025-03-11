3b11680b1d5d93843d4312fc2adad008a98038b3
TylerSchaus <tylerdschaus@shaw.ca>
2025-03-10 19:08:31 -0700

    missed profile annotation

--- Projecta-Backend/src/test/java/com/hackademics/ProjectaBackendApplicationTests.java
+++ Projecta-Backend/src/test/java/com/hackademics/ProjectaBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.hackademics;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProjectaBackendApplicationTests {
 
 	@Test
