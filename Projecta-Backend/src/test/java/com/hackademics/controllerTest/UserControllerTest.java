b92255979ea3ab9230ad171699d7556532aa1e0d
TylerSchaus <tylerdschaus@shaw.ca>
2025-03-10 19:04:56 -0700

    Fixed tests so that all are using the test profile

--- Projecta-Backend/src/test/java/com/hackademics/controllerTest/UserControllerTest.java
+++ Projecta-Backend/src/test/java/com/hackademics/controllerTest/UserControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -26,6 +27,7 @@ import jakarta.transaction.Transactional;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@ActiveProfiles("test")
 class UserControllerTest {
 
     @Autowired
