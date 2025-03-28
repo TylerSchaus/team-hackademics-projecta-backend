1d1db0271c3ba64eeeb3398dcdb4466a432c603f
TylerSchaus <tylerdschaus@shaw.ca>
2025-03-28 13:11:31 -0700

    Finished clean up class

--- Projecta-Backend/src/main/java/com/hackademics/config/DatabaseCleanup.java
+++ Projecta-Backend/src/main/java/com/hackademics/config/DatabaseCleanup.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Component;
 import com.hackademics.repository.CourseRepository;
 import com.hackademics.repository.EnrollmentRepository;
 import com.hackademics.repository.GradeRepository;
+import com.hackademics.repository.LabSectionRepository;
+import com.hackademics.repository.SubjectRepository;
 import com.hackademics.repository.UserRepository;
+import com.hackademics.repository.WaitlistEnrollmentRepository;
 
 @Component
 @ConditionalOnProperty(name = "app.database.cleanup", havingValue = "true")
@@ -26,14 +29,31 @@ public class DatabaseCleanup implements CommandLineRunner {
     @Autowired
     private EnrollmentRepository enrollmentRepository;
 
+    @Autowired
+    private LabSectionRepository labSectionRepository;
+
+    @Autowired
+    private SubjectRepository subjectRepository;
+
+    @Autowired
+    private WaitlistEnrollmentRepository waitlistEnrollmentRepository;
+    
+    @Autowired
+    private WaitlistEnrollmentRepository waitlistRepository;
+
+
     @Override
     public void run(String... args) {
         System.out.println("Cleaning up database...");
         
         // Delete all data in reverse order of dependencies
-        enrollmentRepository.deleteAll();
+        waitlistEnrollmentRepository.deleteAll();
+        waitlistRepository.deleteAll();
+        labSectionRepository.deleteAll();
         gradeRepository.deleteAll();
+        enrollmentRepository.deleteAll();
         courseRepository.deleteAll();
+        subjectRepository.deleteAll();
         userRepository.deleteAll();
         
         System.out.println("Database cleanup completed.");
