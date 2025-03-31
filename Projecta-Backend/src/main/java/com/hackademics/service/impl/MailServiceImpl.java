18c90ccede407ccba46c5d7cb2c1d9cd05c4d91d
TylerSchaus <tylerdschaus@shaw.ca>
2025-03-30 23:34:56 -0700

    Updated ci, hopefully git allows it now

--- Projecta-Backend/src/main/java/com/hackademics/service/impl/MailServiceImpl.java
+++ Projecta-Backend/src/main/java/com/hackademics/service/impl/MailServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hackademics.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -16,36 +17,27 @@ public class MailServiceImpl implements MailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Value("${email.sending.enabled:true}")
+    private boolean emailSendingEnabled;
+
     @Override
     public void sendEnrollmentEmail(EnrollmentResponseDto enrollmentResponseDto, User student) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("hackademicsuniversity@gmail.com");
         message.setTo(student.getEmail());
-        message.setSubject("Course Enrollment Confirmation");
-        message.setText("Dear " + student.getFirstName() + ",\n\n" +
-                "You have been successfully enrolled in " + enrollmentResponseDto.getCourse().getCourseName() + ".\n\n" +
-                "Course Details:\n" +
-                "Course: " + enrollmentResponseDto.getCourse().getCourseName() + "\n" +
-                "Course Number: " + enrollmentResponseDto.getCourse().getCourseNumber() + "\n" +
-                "Term: " + enrollmentResponseDto.getCourse().getTerm() + "\n\n" +
-                "Best regards,\nHackademics Team");
-
+        message.setSubject("Enrollment Confirmation");
+        message.setText("You have been enrolled in the course " + enrollmentResponseDto.getCourse().getCourseName() + " for the term " + enrollmentResponseDto.getCourse().getTerm());
         javaMailSender.send(message);
+        System.out.println("Enrollment email sent to " + student.getEmail());
     }
 
     @Override
     public void sendWaitlistEmail(WaitlistEnrollmentResponseDto waitlistEnrollmentResponseDto, User student) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("hackademicsuniversity@gmail.com");
         message.setTo(student.getEmail());
-        message.setSubject("Waitlist Enrollment Confirmation");
-        message.setText("Dear " + student.getFirstName() + ",\n\n" +
-                "You have been added to the waitlist for " + waitlistEnrollmentResponseDto.getWaitlistResponseDto().getCourse().getCourseName() + ".\n\n" +
-                "Course Details:\n" +
-                "Course: " + waitlistEnrollmentResponseDto.getWaitlistResponseDto().getCourse().getCourseName() + "\n" +
-                "Course Number: " + waitlistEnrollmentResponseDto.getWaitlistResponseDto().getCourse().getCourseNumber() + "\n" +
-                "Term: " + waitlistEnrollmentResponseDto.getWaitlistResponseDto().getCourse().getTerm() + "\n" +
-                "Waitlist Position: " + waitlistEnrollmentResponseDto.getWaitlistPosition() + "\n\n" +
-                "Best regards,\nHackademics Team");
-
+        message.setSubject("Waitlist Confirmation");
+        message.setText("You have been added to the waitlist for the course " + waitlistEnrollmentResponseDto.getWaitlistResponseDto().getCourse().getCourseName() + " for the term " + waitlistEnrollmentResponseDto.getTerm());
         javaMailSender.send(message);
     }
 
@@ -68,4 +60,5 @@ public class MailServiceImpl implements MailService {
         message.setText("You have been removed from the waitlist for the course " + waitlistEnrollmentResponseDto.getWaitlistResponseDto().getCourse().getCourseName() + " for the term " + waitlistEnrollmentResponseDto.getTerm());
         javaMailSender.send(message);
     }
+
 }
